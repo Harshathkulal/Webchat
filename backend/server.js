d1f9c951ad100e2e2650c7e52e805f1a337d0d97
harshath <harshathmkulal@email.com>
2024-03-25 16:58:30 +0530

    webChat version 0.1

---
+++ backend/server.js
@@ -0,0 +1,41 @@
+//package
+import path from "path";
+import express from "express";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+
+//file imports
+import authRoutes from "./routes/auth.routes.js";
+import messageRoutes from "./routes/message.routes.js";
+import userRoutes from "./routes/user.routes.js";
+import connectToMongoDB from "./db/MongoDBConnect.js";
+import { app, server } from "./socket/socket.js";
+
+
+//variables
+const PORT=process.env.PORT || 5000;
+
+const __dirname = path.resolve();
+
+dotenv.config()
+
+
+//middleware
+app.use(express.json()); //parse incoming requests with json payload (from req.body)
+app.use(cookieParser());
+
+app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
+app.use("/api/users", userRoutes);
+
+
+app.use(express.static(path.join(__dirname, "/client")));
+
+app.get("/", (req, res) => {
+	res.sendFile(path.join(__dirname, "client", "index.html"));
+});
+
+server.listen(PORT, () => {
+	connectToMongoDB();
+	console.log(`Server Running on port ${PORT}`);
+});
